5d4f999dcfdbb8248cfbfd861cc1849d6dc9ad64
Julia Komarnitska <juliaowlghost@gmail.com>
2022-06-08 19:54:13 +0300

    add html and js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let num = 123456; //number
+console.log("value: " + num + "; type: " + typeof num);
+
+let info = 'IT School Hillel'; //string
+console.log("value: " + info + "; type: " + typeof info);
+
+let decision = false; //boolean
+console.log("value: " + decision + "; type: " + typeof decision);
+
+alert('keep calm and code javascript');
